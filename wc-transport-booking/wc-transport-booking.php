ff46c23607b7e1c1d029478d4328d6e1509bedfd
Martin Bo kristensen Grønholdt <oblivion@vtestserv>
2016-07-31 13:12:09 +0200

    First changes for booking plugin

--- wc-transport-booking/wc-transport-booking.php
+++ wc-transport-booking/wc-transport-booking.php
@@ -1,32 +1,19 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://example.com
- * @since             1.0.0
- * @package           Plugin_Name
- *
- * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Plugin Name:       WooCommerce booking
+ * Plugin URI:        http://https://github.com/deadbok/WooCommerce-Booking/
+ * Description:       WooCommerce transport booking extension
+ * Version:           0-0-2
+ * Author:            Martin Bo Kristensen Grønholdt
+ * Author URI:        http://groenholdt.net
+ * Text Domain:       wc-booking
  * Domain Path:       /languages
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if ( ! defined( 'WPINC' ) ) 
+{
 	die;
 }
 
